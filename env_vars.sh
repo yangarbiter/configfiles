be2b4327652aa9f869b5b015214593f663012315
yangarbiter <yangarbiter@users.noreply.github.com>
2021-06-18 09:03:49 -0700

    Update env_vars.sh

--- env_vars.sh
+++ env_vars.sh
@@ -10,8 +10,8 @@ export LC_ALL=en_US.UTF-8
 export DOTFILE_BASEDIR=/home/arbiter/configfiles
 export PYENV_ROOT=/home/arbiter/.pyenv
 export EDITOR=vim
-export SHELL=zsh
-export PYENV_SHELL=zsh
+export SHELL=/bin/zsh
+export PYENV_SHELL=/bin/zsh
 
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/cuda-10.0/lib64
